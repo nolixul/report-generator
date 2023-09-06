3cb3a3cf0366252c12de42d013523be9c4e3f908
nolixul <nadia.kaye@gmail.com>
2023-09-06 22:10:48 +0100

    Add csv generator

--- admin/src/index.js
+++ admin/src/index.js
@@ -2,6 +2,27 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const config = require("config")
 const request = require("request")
+const R = require("ramda")
+
+const csvHeadings = [
+  "User", "First Name", "Last Name", "Date", "Holding", "Value"
+]
+
+const separateHoldings = (investment) => {
+  return investment.holdings.map((holding) => {
+    return {
+      ...investment, holding
+    } 
+  })
+}
+
+const fieldGetters = [inv => inv.userId, inv => inv.firstName, inv => inv.lastName, inv => inv.date, inv => getHoldings(inv), inv => getValue(inv)]
+
+const getValue = (inv) => {
+  return inv.investmentTotal * inv.holding.investmentPercentage
+}
+
+// const getHoldings = () => {return "placeholder"}
 
 const app = express()
 
@@ -13,7 +34,22 @@ app.get("/investments/report", (req, res) => {
       console.error(e)
       res.send(500)
     } else {
-      // res.send()
+      
+      const parsedInvestments = JSON.parse(investments)
+
+      const csvRows = parsedInvestments.map(separateHoldings).flat().map(investment => fieldGetters.map(fieldGetter => fieldGetter(investment)))
+
+      csvRows.unshift(csvHeadings)
+
+      const csvContent = csvRows.map(row => row.join(',') + '/n')
+
+      const investmentsReport = new Blob([csvContent], { type: 'text/csv;charset=utf-8,' })
+
+      // content type should equal text/csv
+      // csv should be returned as text
+
+      // forward to exports
+      res.send(200)
     }
   })
 })
